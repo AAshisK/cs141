ad54c5dc29332e0c0362e918211064a9a612d39d
AAshiK <42205519+AAshisK@users.noreply.github.com>
2018-08-16 21:12:20 +0530

    Add files via upload

---
+++ thirdweek.cpp
@@ -0,0 +1,48 @@
+//include library
+#include <iostream>
+using namespace std;
+int main()
+//dont know what i am doing but still trying
+{
+//declaaring variables
+int a;
+int b;
+int result;
+// mathematical processes
+//starting with addition
+a=11;
+b=13;
+result = a+b;
+//will see if the result comes out
+cout<<result<<endl;
+//lets see what happens...
+//declaring some more variables
+int c,d;
+int resulta;
+//then subtraction
+c=24;
+d=14;
+resulta = c-d;
+//lets try this out and see what happens...
+cout<<resulta<<endl ;
+//now as wverything is working fine will try out multiplication
+int e,f,resultef;
+e= 33;f= 34;
+resultef=e*f;
+cout<<resultef<<endl;
+float g,h,resultgh;
+g=34;h=18;
+resultgh=g/h;
+cout<<resultgh<<endl;
+//modulus means the remainder of the division and after searching i found out the symbol so let's go yeah!!
+int i,j,resultij;
+i=5;
+j=2;
+resultij= i%j;
+cout<<resultij<<endl;
+
+
+
+return 0;
+}
+
---
+++ thirdweekfirstquestion.cpp
@@ -0,0 +1,21 @@
+//include library
+#include <iostream>
+using namespace std;
+int main()
+//hell yeah
+{
+//allocating variables and finding size
+int a=11;
+char b='9';
+float c=11.11;
+double d=1111111;
+bool e=1;
+cout<<"size of the integer in bytes ";
+cout<<sizeof(a)<<endl;
+cout<<sizeof(b)<<endl;
+cout<<sizeof(c)<<endl;
+cout<<sizeof(d)<<endl;
+cout<<sizeof(e)<<e<<endl;
+return 0;
+
+}
